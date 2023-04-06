fbfa6a150772d4a7b82bba708e3bba1330964d55
Ashutosh-s07 <sahuashutosh248@gmail.com>
2023-04-07 01:24:54 +0530

    GFG Kth element of two sorted Arrays :Added

--- KthelementoftwosortedArrays.java
+++ KthelementoftwosortedArrays.java
@@ -10,24 +10,30 @@ public class KthelementoftwosortedArrays {
         System.out.println(kthElement(arr1, arr2, n, m, k));
     }
     static long kthElement( int arr1[], int arr2[], int n, int m, int k) {
-        int s=0,e=0;
-        int p=0;
-        int[] ans = new int[n+m];
-
-        while(s<n && e<m){
-            if(arr1[s]<arr2[e]) ans[p++]=arr1[s++];
-            else ans[p++]=arr2[e++];
+         if(m > n) {
+        return kthElement(arr2, arr1, m, n, k); 
+    }
+        
+    int low = Math.max(0,k-m), high = Math.min(k,n);
+        
+    while(low <= high) {
+        int cut1 = (low + high) >> 1; 
+        int cut2 = k - cut1; 
+        int l1 = cut1 == 0 ? Integer.MIN_VALUE : arr1[cut1 - 1]; 
+        int l2 = cut2 == 0 ? Integer.MIN_VALUE : arr2[cut2 - 1];
+        int r1 = cut1 == n ? Integer.MAX_VALUE : arr1[cut1]; 
+        int r2 = cut2 == m ? Integer.MAX_VALUE : arr2[cut2]; 
+            
+        if(l1 <= r2 && l2 <= r1) {
+            return Math.max(l1, l2);
         }
-        if(s<n){
-            while(s<n){
-                ans[p++]=arr1[s++];
-            }
+        else if (l1 > r2) {
+            high = cut1 - 1;
         }
-        if(e<m){
-            while(e<m){
-                ans[p++]=arr2[e++];
-            }
+        else {
+            low = cut1 + 1; 
         }
-        return ans[k-1];
+    }
+    return -1;
     }
 }

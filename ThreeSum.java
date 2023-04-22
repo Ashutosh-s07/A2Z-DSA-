bc02ce2580d93cb5b8298d4294ebc68a88fe2d32
Ashutosh-s07 <104490701+Ashutosh-s07@users.noreply.github.com>
2023-04-22 14:19:29 +0530

    Add files via upload

---
+++ ThreeSum.java
@@ -0,0 +1,42 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class ThreeSum{
+    public static void main(String[] args) {
+        int[] nums = {-1,0,1,2,-1,-4};
+        threeSum(nums);
+    }
+    static List<List<Integer>> threeSum(int[] num) {
+        
+        Arrays.sort(num);
+        List<List<Integer>> res = new ArrayList<>();
+
+
+        for (int i = 0; i < num.length - 2; i++) {
+
+            if (i == 0 || (i > 0 && num[i] != num[i - 1])) {
+
+                int lo = i + 1, hi = num.length - 1, sum = 0 - num[i];
+
+                while (lo < hi) {
+                    if (num[lo] + num[hi] == sum) {
+                        List < Integer > temp = new ArrayList < > ();
+                        temp.add(num[i]);
+                        temp.add(num[lo]);
+                        temp.add(num[hi]);
+                        res.add(temp);
+
+                        while (lo < hi && num[lo] == num[lo + 1]) lo++;
+                        while (lo < hi && num[hi] == num[hi - 1]) hi--;
+
+                        lo++;
+                        hi--;
+                    } else if (num[lo] + num[hi] < sum) lo++;
+
+                    else hi--;
+                }
+            }
+        }
+        return res;
+    }
+}

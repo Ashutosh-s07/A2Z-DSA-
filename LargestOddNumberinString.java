1ba1bbb960114756c867ade604f2af74ae1078b0
Ashutosh-s07 <sahuashutosh248@gmail.com>
2023-05-02 18:13:37 +0530

    git ignore file added

--- LargestOddNumberinString.java
+++ LargestOddNumberinString.java
@@ -13,8 +13,9 @@ public class LargestOddNumberinString {
         for(int i=1;i<=num.length();i++){
             // System.out.println(num.charAt(n-i)); 
             // System.out.println(num.substring(0, n-i));
-            if(num.charAt(n-i)=='1' || num.charAt(n-i)=='3' || num.charAt(n-i)=='5' || num.charAt(n-i)=='7' || num.charAt(n-i)=='9'){
-                
+            // if(num.charAt(n-i)=='1' || num.charAt(n-i)=='3' || num.charAt(n-i)=='5' || num.charAt(n-i)=='7' || num.charAt(n-i)=='9'){
+            if((num.charAt(n-i)%2)!=0){
+            
                 return num.substring(0, n-i+1);
             }
         }
